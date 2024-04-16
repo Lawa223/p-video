9ee0a6ace38162d4b04d0ffd7f4b5b1bb7f7c4c6
unknown <Lawal223>
2024-04-16 09:34:30 +0100

    change name

--- video.js
+++ video.js
@@ -3,7 +3,7 @@ function changeContent(){
     let text1 = document.getElementById("text1")
     let text2 = document.getElementById("text2")
     // let text3 = document.getElementById("text3")
-    video1.src = "spontaneous Generation.mp4"
+    video1.src = "Spontaneous Generation.mp4"
     text1.style.backgroundColor = "red"
     text2.textContent = "Boldlink technology solution"
     content.style.color = "green"
